b6de2ba8aeb84ce7b1b7349ec58fd31bddce6e07
Stanley Lai <stanley.lai@telus.com>
2023-01-30 20:19:00 -0800

    cypress Feature Spec: product Details

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,24 @@
+describe('Home Page', () => {
+
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("should navigate to product detail page", () => {
+    
+    cy.get(":nth-child(1) > a > img")
+      .click()
+  });
+})
